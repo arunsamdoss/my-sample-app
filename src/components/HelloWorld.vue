afbe02ff6b5be2ab7f298c10ced7d16213688b4f
sam <arunsamdoss@gmail.com>
2021-02-27 17:14:34 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,144 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
 
+//Login.vue
+
+<template>
+  <section class="section-container">
+    <v-row class="signin">
+      <v-col cols="8" class="left">
+        <h1>Welcome to my site</h1>
+      </v-col>
+      <v-col cols="4" class="right">
+        <h2>LOGIN</h2>
+        <validation-observer ref="observer">
+          <v-form @submit.prevent="submit">
+            <validation-provider v-slot="{ errors }" name="Name" rules="required|email">
+              <v-text-field
+                v-model="email"
+                :error-messages="errors"
+                label="Email"
+                required
+                outlined
+                dark
+                filled
+                dense
+              ></v-text-field>
+            </validation-provider>
+            <validation-provider v-slot="{ errors }" name="email" rules="required">
+              <v-text-field
+                v-model="password"
+                :error-messages="errors"
+                label="Password"
+                :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPass = !showPass"
+                required
+                outlined
+                dense
+                dark
+                filled
+                :type="showPass ? 'text' : 'password'"
+              ></v-text-field>
+            </validation-provider>
+            <div class="text-center">
+              <v-btn class="signin-btn" type="submit" rounded color="white" dark>
+                Sign In
+              </v-btn>
+            </div>
+          </v-form>
+        </validation-observer>
+      </v-col>
+    </v-row>
+  </section>
+</template>
 <script>
+import { required, email } from 'vee-validate'
+import { extend, ValidationProvider, setInteractionMode, ValidationObserver } from 'vee-validate'
+
+setInteractionMode('eager')
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty'
+})
+
+extend('email', {
+  ...email,
+  message: 'Email must be valid'
+})
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
+  data: () => ({
+    email: '',
+    password: null,
+    showPass: false
+  }),
+  computed: {
+    params() {
+      return {
+        email: this.email,
+        password: this.password
+      }
+    }
+  },
+  methods: {
+    async submit() {
+      const valid = await this.$refs.observer.validate()
+      if (valid) {
+        this.login(this.params) // action to login
+      }
+    },
+    clear() {
+      // you can use this method to clear login form
+      this.email = ''
+      this.password = null
+      this.$refs.observer.reset()
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+/* ./assets/styles.scss */
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.section-container {
+  padding: 20px;
+  margin: 20px;
+  background: #fff;
+  width: 100%;
+  box-shadow: 0 0 1px 1px rgba($color: #000000, $alpha: 0.1);
+  box-sizing: border-box;
+  .signin {
+    padding: 0;
+    max-width: 1000px;
+    margin: 0 auto;
+    min-height: 600px;
+    box-shadow: 0 0 1px 1px rgba($color: #000000, $alpha: 0.1);
+    .left {
+      padding: 30px;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+      display: flex;
+      color: #30ac7c;
+      background-color: #f9f9f9;
+    }
+    .right {
+      padding: 30px;
+      box-sizing: border-box;
+      background: #30ac7c;
+      color: #fff;
+      h2 {
+        text-align: center;
+        margin: 30px 0;
+      }
+      .signin-btn {
+        width: 100%;
+        color: #30ac7c;
+      }
+    }
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
